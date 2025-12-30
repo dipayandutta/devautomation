1c3a04d7c5703aa35f782c1bc677de8d8df21f56
Dipayan Dutta <dipayandutta@Dipayans-MacBook-Air.local>
2025-12-30 16:33:51 +0530

    Added More Codes

--- pythonKube.py
+++ pythonKube.py
@@ -1,5 +1,10 @@
 from kubernetes import client, config
 
+SYSTEM_NAMESPACES = {
+	"kube-system",
+	"kube-public",
+	"kube-node-lease"
+}
 
 def getKubeConfig():
 	try:
@@ -16,6 +21,63 @@ def getKubeConfig():
 	apps_v1 = client.AppsV1Api()
 	return v1,apps_v1
 
+
+def getNodeDetails(v1):
+	nodes = v1.list_node().items
+	for node in nodes:
+		nodeStatus = [c.status for c in node.status.conditions if c.type=="Ready"][0]
+		print("CPU: ",node.status.capacity.get("CPU"))
+		print("OS: ",node.status.node_info.os_image)
+		print("Runtime Version: ",node.status.node_info.container_runtime_version)
+	print(nodeStatus)
+	if nodeStatus == True:
+		print("Nodes are Ready")
+
+def getNamespaces(v1):
+	print("List of all Namespaces")
+	print("------------------------")
+	namespaces = v1.list_namespace().items
+	for namespace in namespaces:
+		print(namespace.metadata.name)
+
+def getPods(v1):
+	#pods = v1.list_pod_for_all_namespaces()
+	#print("List all Pods")
+	#print("---------------")
+	#for p in pods.items:
+	#	print(f"{p.metadata.namespace}--> {p.metadata.name} [{p.status.phase}]")
+	pods = v1.list_pod_for_all_namespaces().items
+
+	systemPods = []
+	userPods = []
+	
+	for pod in pods:
+		entry = f"{pod.metadata.namespace}-->{pod.metadata.name}[{pod.status.phase}]"
+		entryCreationTimeStamp = pod.metadata.creation_timestamp
+
+		if pod.metadata.namespace in SYSTEM_NAMESPACES:
+			systemPods.append(entry)
+		else:
+			userPods.append((entry,entryCreationTimeStamp))
+
+
+		print("List all Pods")
+		print("---------------")
+		print("System Pods")
+		print("----------------")
+		for pod in systemPods:
+			print(pod)
+		print("User Created Pods")
+		print("------------------")
+		for pod,timeStamp in userPods:
+			print(f"{pod} | CreatedAt = {timeStamp}")
+		
+def getEvents(v1):
+	events = v1.list_event_for_all_namespaces()
+	print("List all Events")
+	print("------------------")
+	for event in events.items:
+		print(f"{event.metadata.namespace} -> {event.reason}: {event.message}")
 
 def detectClusterType(v1):
 	nodes = v1.list_node().items
@@ -50,6 +112,9 @@ def main():
 	detectClusterType(v1)
 	getSVC(v1)
 	getDeployment(apps_v1)
-
+	getNodeDetails(v1)
+	getPods(v1)
+	getEvents(v1)
+	getNamespaces(v1)
 if __name__ == '__main__':
 	main()
